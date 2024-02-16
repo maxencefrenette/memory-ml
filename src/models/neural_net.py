07ae8e870a5792d84cd3a1a4d0626a7df36979d6
Maxence Frenette <maxence.frenette@gmail.com>
2024-02-15 22:00:50 -0800

    Add more hyperparameters

--- src/models/neural_net.py
+++ src/models/neural_net.py
@@ -4,24 +4,46 @@ import lightning as L
 from torchmetrics.classification import BinaryCalibrationError
 
 
+class NNHiddenLayer(nn.Module):
+    def __init__(self, size: int, dropout: float):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Linear(size, size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.layer(x)
+
+
 class NNMemoryModel(L.LightningModule):
     def __init__(
-        self, learning_rate: float, reviews_history_size: int, hidden_size: int
+        self,
+        learning_rate: float,
+        reviews_history_size: int,
+        hidden_layer_size: int,
+        hidden_layer_num: int,
+        dropout: float,
     ):
         super().__init__()
         self.save_hyperparameters()
 
         self.model = nn.Sequential(
-            nn.Linear(2 + 4 * reviews_history_size, hidden_size),
+            nn.Linear(2 + 4 * reviews_history_size, hidden_layer_size),
+            nn.Dropout(dropout),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, 1),
+            *[
+                NNHiddenLayer(hidden_layer_size, dropout)
+                for _ in range(hidden_layer_num)
+            ],
+            nn.Linear(hidden_layer_size, 1),
             nn.Sigmoid(),
         )
 
         self.loss_fn = nn.BCELoss()
         self.calibration_fn = BinaryCalibrationError(n_bins=20, norm="l2")
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
 
     def forward(self, x):
         return self.model(x)
@@ -46,4 +68,4 @@ class NNMemoryModel(L.LightningModule):
         self.log("test/calibration", self.calibration_fn(pred, y))
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
+        return self.optimizer
